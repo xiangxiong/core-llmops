a7440a3808730e8c4a9631db7b444d62ca7565f2
xiangxiong <xx090304@163.com>
2025-12-10 23:52:28 +0800

    Update Flask application with README and requirements

---
+++ flask/README.md
@@ -0,0 +1,20 @@
+flask 教程:
+https://osgeo.cn/flask/
+
+python 教程:
+https://docs.python.org/zh-cn/3/tutorial/index.html
+
+创建虚拟环境:
+```
+python -m venv venv
+```
+激活虚拟环境:
+
+```
+source venv/bin/activate
+```
+
+生成requirements.txt: 文件
+```
+pip freeze > requirements.txt
+```
--- flask/main.py
+++ flask/main.py
@@ -1,11 +1,35 @@
 # 导入flask
+from flask import request
 from flask import Flask
+from flask import make_response
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>H22ello, World! eee</p>"
-    
+    return "<p>H22ello, World! eee333</p>"
+
+@app.route("/cookie")
+def cookie():
+    response = make_response("<p>设置cookie222</p>")
+    expires_time = datetime.datetime.now() + datetime.timedelta(days=1)
+    response.set_cookie("username", "h22ello", expires=expires_time);
+    response.set_cookie("password", "h22ello", expires=expires_time);
+
+    return response;
+
+@app.route("/get_cookie")
+def get_cookie():
+    username = request.cookies.get("username")
+    print(username);
+    cookies = request.cookies
+    cookies_dict = request.cookies.to_dict()
+    print(cookies_dict);
+    for k,v in cookies_dict.items():
+        print(k,v)
+
+    return f"username: {username}"
+
 if __name__ == '__main__':
     app.run(debug=True)
