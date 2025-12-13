dd24f3de4ca31c281104101c08bf3d5ac01c4af1
xiangxiong <xx090304@163.com>
2025-12-13 11:08:30 +0800

    fix: bug

--- flask/mvc/controller/user.py
+++ flask/mvc/controller/user.py
@@ -1,6 +1,14 @@
 from flask import Blueprint
+from flask import request
 
 user = Blueprint('user', __name__)
+
+@user.before_request
+def before():
+    if request.path.startswith('/v'):
+        pass
+    else:
+        return "<p>请登录</p>"
 @user.route("/user/add")
 def add_user():
     return "<p>添加用户成功</p>"
@@ -8,3 +16,7 @@ def add_user():
 @user.route("/user/update")
 def update_user():
     return "<p>更新用户成功</p>"
+
+@user.route("/v/user/info")
+def get_info():
+    return "<p>获取用户信息成功</p>"
--- flask/mvc/main2.py
+++ flask/mvc/main2.py
@@ -1,16 +1,15 @@
 # 导入flask
-from flask import Flask
-from controller.user import user
-from controller.article import article
-from flask import session
-from flask import request
+from flask import Flask;
+from controller.user import user;
+from controller.article import article;
+from flask import session;
+from flask import request;
 import os;
 
 app = Flask(__name__)
 
 # 启用session
 app.config["SECRET_KEY"] = os.urandom(24); # 生成一个随机的24位字节, 作为session的密钥
-
 app.register_blueprint(user)
 app.register_blueprint(article) 
 
@@ -28,9 +27,23 @@ def update_user():
 def before():
     # 获取用户的url
     url = request.path;
-    if url == '/login':
-        # return "<p>登录成功11</p>"
+    print(url);
+
+    # 白名单
+    pass_paths = [
+        '/',
+        '/login',
+        '/reg'
+    ];
+
+    # 定义一个可通过的后缀名
+    suffix = url.endswith("png") or url.endswith("jpg") or url.endswith("css") or url.endswith("js")
+
+    if url in pass_paths or suffix:
         pass
+    # if url == '/login':
+    #     # return "<p>登录成功11</p>"
+    #     pass
     else:
         if_login = session.get('isLogin')
         if  if_login != True:
